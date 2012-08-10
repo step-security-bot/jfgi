de097b05c2163192272cc07a7a412de4a2b2ecef
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-10 16:46:38 +0500

    tentative changes for a rudimentary counter

--- jfgi.rb
+++ jfgi.rb
@@ -7,18 +7,18 @@ require 'haml'
 hit_counter ||= 0
 
 get '/' do
-	haml 	:index
+	haml 	:index, :locals => { :hit_counter => hit_counter }, :format => :html5
         hit_counter += 1
 end
 
 get '/search/:query' do
-	haml	:index
+	haml	:index, :format => :html5
 	sleep 15
 	redirect to("http://www.google.com/search?q=#{params[:query]}")
         hit_counter += 1
 end
 
 get '/info' do
-	haml :info
+	haml :info, :format => :html5
         hit_counter += 1
 end
