87ff6a137a140d2fbc285fd3d76e9077d31b9a2e
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-10 15:09:22 +0500

    Adding basic counter functionaity

--- jfgi.rb
+++ jfgi.rb
@@ -4,16 +4,21 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+hit_counter ||= 0
+
 get '/' do
 	haml 	:index
+        hit_counter += 1
 end
 
 get '/search/:query' do
 	haml	:index
 	sleep 15
 	redirect to("http://www.google.com/search?q=#{params[:query]}")
+        hit_counter += 1
 end
 
 get '/info' do
 	haml :info
+        hit_counter += 1
 end
