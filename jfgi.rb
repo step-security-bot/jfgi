58f97039999aa81eb6a8aca6bd3ed0e0a9901787
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-11 03:01:28 +0500

    Added the legacy redirect method to the search page

--- jfgi.rb
+++ jfgi.rb
@@ -5,7 +5,7 @@ require 'sinatra'
 require 'haml'
 
 hit_counter ||= 0
-
+time_delay = 15
 get '/' do
         hit_counter += 1
 	haml 	:index, :locals => { :hit_counter => hit_counter }, :format => :html5
@@ -13,9 +13,9 @@ end
 
 get '/search/:query' do
         hit_counter += 1
-	haml	:search, :locals => { :query => params[:query], :hit_counter => hit_counter}, :format => :html5
-	sleep 15
-	redirect to("http://www.google.com/search?q=#{params[:query]}")
+	haml	:search, :locals => { :query => params[:query], :hit_counter => hit_counter, :time_delay => time_delay}, :format => :html5
+#	sleep 15
+#	redirect to("http://www.google.com/search?q=#{params[:query]}")
 end
 
 get '/info' do
