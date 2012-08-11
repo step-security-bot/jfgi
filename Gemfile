13e79bf7b84aaed567db219b907a3040f6fa8191
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-11 19:07:03 +0500

    Reworked it to use datamapper and a database

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'haml', '>= 3.2.0.beta.1'
+gem 'datamapper'
--- jfgi.rb
+++ jfgi.rb
@@ -3,21 +3,40 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require './helpers'
+require 'datamapper'
 
-hit_counter = update_counter 
+class Counter
+  include DataMapper::Resource
+  property :id, Serial
+  property :time, DateTime
+  property :hits, Integer
+end
+
+def update_counter(counter)
+  counter.hits += 1
+  counter.save
+end
+
+configure do
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] ||'postgres://localhost/mydb'))
+  DataMapper.autoupgrade!
+end
+
+
+counter = Counter.get(1) || Counter.create(:time => Time.now, :hits => 1) 
+hits_counter = counter.hits.to_i
 time_delay = 15
 get '/' do
-        update_counter
+        update_counter(counter)
 	haml 	:index, :locals => { :hit_counter => hit_counter }, :format => :html5
 end
 
 get '/search/:query' do
-        update_counter
+        update_counter(counter)
 	haml	:search, :locals => { :query => params[:query], :hit_counter => hit_counter, :time_delay => time_delay}, :format => :html5
 end
 
 get '/info' do
-        update_counter
+        update_counter(counter)
 	haml :info, :format => :html5
 end
