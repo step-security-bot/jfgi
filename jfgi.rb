6922f8ae3e50ddf9406e57e0393c4b5c76b5f1f1
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-11 13:55:07 +0500

    Added a new method to track hits

--- jfgi.rb
+++ jfgi.rb
@@ -7,18 +7,24 @@ require 'haml'
 hit_counter ||= ENV['HITS']
 time_delay = 15
 get '/' do
-        ENV['HITS'] += 1
+        update_counter
 	haml 	:index, :locals => { :hit_counter => hit_counter }, :format => :html5
 end
 
 get '/search/:query' do
-        ENV['HITS'] += 1
+        update_counter
 	haml	:search, :locals => { :query => params[:query], :hit_counter => hit_counter, :time_delay => time_delay}, :format => :html5
 #	sleep 15
 #	redirect to("http://www.google.com/search?q=#{params[:query]}")
 end
 
 get '/info' do
-        ENV['HITS'] += 1
+        update_counter
 	haml :info, :format => :html5
+end
+
+def update_counter
+  current_count = ENV['HITS'].to_i
+  new_count = current_count + 1
+  ENV['HITS'] = new_count.to_s
 end
