b0a811fb0b2fd6005dfbac0d59f2a62fe606f1f0
Ali Sajid Imami <ASImami@nhmag.org>
2012-08-11 17:35:57 +0500

    Tweaking the ENV reading code

--- jfgi.rb
+++ jfgi.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-hit_counter ||= ENV['HITS']
+hit_counter = update_counter 
 time_delay = 15
 get '/' do
         update_counter
@@ -14,8 +14,6 @@ end
 get '/search/:query' do
         update_counter
 	haml	:search, :locals => { :query => params[:query], :hit_counter => hit_counter, :time_delay => time_delay}, :format => :html5
-#	sleep 15
-#	redirect to("http://www.google.com/search?q=#{params[:query]}")
 end
 
 get '/info' do
@@ -27,4 +25,5 @@ def update_counter
   current_count = ENV['HITS'].to_i
   new_count = current_count + 1
   ENV['HITS'] = new_count.to_s
+  return new_count
 end
