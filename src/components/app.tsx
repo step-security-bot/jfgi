afb42a54de5d2a57866c4244def759a8e7a66842
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-05 01:41:01 -0500

    More project restructuring

---
+++ src/components/app.tsx
@@ -0,0 +1,22 @@
+/** @format */
+
+import { app } from "../utils/firebase";
+
+export function App() {
+  return (
+    <>
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  );
+}
