f1f9e1394ccccca43ede29495a634ccdcaa80532
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-05 01:43:08 -0500

    Impklemented ttailwind into the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import { render } from "preact";
-import { App } from "./app";
-import "./index.css";
+import { App } from "./components/app";
+import "./assets/css/index.css";
 
 render(<App />, document.getElementById("app")!);
