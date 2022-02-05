3ab54d5d330c9d9e45741ff237a06484e674eb2c
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-05 00:19:11 -0500

    Added firebase configuration

---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+// Import the firebase module and necessary APIs
+import { initializeApp } from 'firebase';
+import { getAnalytics } from 'firebase/analytics';
+import { getPerformance } from 'firebase/performance';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+}
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const performance = getPerformance(app);
+export const firestore = getFirestore(app);
