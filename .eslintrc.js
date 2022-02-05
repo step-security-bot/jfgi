0b8a41152a51990699cc069a4bc480078482aa43
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-05 01:30:58 -0500

    Additional constttraints on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
+/** @format */
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "plugin:prettier/recommended",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+    "prettier/babel",
+    "prettier/react",
+    "prettier/standard",
+  ],
+  ignorePatterns: ["build/"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
