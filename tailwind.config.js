8af0f8f22816e2f7d7058f50d9520a301ab3ef1d
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-05 01:42:57 -0500

    Added tailwind support to the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
