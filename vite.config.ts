7f6cf809d22f832eda52d75b0da3db204d4711e9
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-06 00:41:26 -0500

    Added compatibilitty layers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+/** @format */
+
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
-})
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom": "preact/compat",
+    },
+  },
+});
