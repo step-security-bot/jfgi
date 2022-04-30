6d89c3fcd1f8b0606ffb5605f02dd4a4ced3c9d7
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-04-29 23:51:58 -0400

    Added the timer component

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="timer" class="timer">
-    <p class="text-xl justify-center">
-      {{ time }}
-    </p>
+    <p class="text-xl justify-center">{{ timeout }} seconds remaining</p>
+    <p>Current URL: {{ url }}</p>
+    <p>Current Query: {{ query }}</p>
   </div>
 </template>
 
@@ -11,13 +11,33 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Timer",
+  data() {
+    return {
+      timeout: 30,
+      url: window.location.href,
+    };
+  },
   computed: {
-    time() {
-      return Date.now();
+    query(): string {
+      const url = this.url;
+      const query = url.split("/").pop();
+      if (query) {
+        return query;
+      } else {
+        return "";
+      }
     },
   },
   mounted() {
-    this.time;
+    setInterval(() => this.tick(), 1000);
+  },
+  methods: {
+    tick() {
+      this.timeout--;
+      if (this.timeout === 0) {
+        window.location.href = `https://www.google.com/search?q=${this.query}`;
+      }
+    },
   },
 });
 </script>
