88cd07e704ac77a0e0985801e46aca22e59d9678
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-05-02 23:04:03 -0500

    Removed an unnecessary line

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import Header from "./components/Header.vue";
 import Timer from "./components/Timer.vue";
 import Reprimand from "./components/Reprimand.vue";
-import Header1 from "./components/Header.vue";
 import VisitorCount from "./components/VisitorCount.vue";
 </script>
 
