a132175bf5fa2ff085e917fc2546410c1ca0b58d
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-05-02 23:03:20 -0500

    Moved testing to happy-dom for performance

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
   },
   resolve: {
     alias: {
