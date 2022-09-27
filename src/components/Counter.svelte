b3ad9f7f7ff26e04fadc05566fd0a0e98cb8f5fa
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-27 16:36:06 -0400

    feat: link the counter to the db

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,5 +1,20 @@
-<script>
-	let count = 1000;
+<script lang="ts">
+	import { PrismaClient } from '@prisma/client';
+	const prisma = new PrismaClient();
+
+	async function get_visitor_count() {
+		let count = await prisma.visits.findUnique({
+			where: {
+				id: 1
+			},
+			select: {
+				count: true
+			}
+		});
+		return count;
+	}
+
+	let count = get_visitor_count();
 </script>
 
 <div class="flex justify-center p-4" id="Counter">
