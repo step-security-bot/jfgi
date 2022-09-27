d5875632f9876b86bfc8f55c84c787fe1b0fbdad
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-09-27 15:54:55 -0400

    feat: add the counter component stub

---
+++ src/components/Counter.svelte
@@ -0,0 +1,7 @@
+<script>
+	let count = 1000;
+</script>
+
+<div class="flex justify-center p-4" id="Counter">
+	<p class="text-lg font-bold">Count: {count}</p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,12 @@
 	import Title from '$components/Title.svelte';
 	import Reprimand from '$components/Reprimand.svelte';
 	import Image from '$components/Image.svelte';
+	import Counter from '$src/components/Counter.svelte';
 </script>
 
 <div class="flex flex-col">
 	<Title />
 	<Image />
 	<Reprimand />
+	<Counter />
 </div>
