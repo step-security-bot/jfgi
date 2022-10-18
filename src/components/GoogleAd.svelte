76b8e40f248185a51b081446c85e54ad2a8534d0
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-10-18 15:17:37 -0400

    feat: added the google ad component

---
+++ src/components/GoogleAd.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  console.log('Google Ad Loaded');
+</script>
+
+<div class="object-center" id="googlead">
+  <img src="https://via.placeholder.com/150" class="p-4 mx-auto my-auto" alt="Placeholder" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-	import Title from '$components/Title.svelte';
-	import Reprimand from '$components/Reprimand.svelte';
-	import Image from '$components/Image.svelte';
-	import Counter from '$src/components/Counter.svelte';
+  import Title from '$components/Title.svelte';
+  import Reprimand from '$components/Reprimand.svelte';
+  import Image from '$components/Image.svelte';
+  import Counter from '$src/components/Counter.svelte';
+  import GoogleAd from '$src/components/GoogleAd.svelte';
 </script>
 
 <div class="flex flex-col">
-	<Title />
-	<Image />
-	<Reprimand />
-	<Counter />
+  <Title />
+  <Image />
+  <Reprimand />
+  <GoogleAd />
+  <Counter />
 </div>
