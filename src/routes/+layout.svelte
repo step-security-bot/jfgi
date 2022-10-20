8c2fca044177cb83d0f7aed43713074355e653be
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-10-20 15:55:31 -0400

    feat: change component hierarchy
    BREAKING CHANGE:

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts" type="module">
   import '$src/app.css';
+  import Title from '$components/Title.svelte';
+  import Reprimand from '$components/Reprimand.svelte';
+  import Image from '$components/Image.svelte';
+  import VisitorCounter from '$src/components/VisitorCounter.svelte';
+  import GoogleAd from '$src/components/GoogleAd.svelte';
 </script>
 
-<slot />
+<div class="flex flex-col">
+  <Title />
+  <Image />
+  <Reprimand />
+  <slot />
+  <GoogleAd />
+  <VisitorCounter />
+</div>
---
+++ src/routes/[query]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  let mySearchQuery = $page.params.query;
+</script>
+
+<div class="flex justify-center p-4">
+  <h1 class="font-extralight text-3xl">Search Google for <code>{mySearchQuery}</code></h1>
+</div>
