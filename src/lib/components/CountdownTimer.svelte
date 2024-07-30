a883fd658b06df53e72e4f1ae4eaff5546073f3f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-07-30 17:34:07 -0400

    feat: add a countdown timer component
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

---
+++ src/lib/components/CountdownTimer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let timer: number = 10;
+
+  let time = timer;
+
+  const interval = setInterval(() => {
+    time -= 1;
+    if (time === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+</script>
+
+<h1>{time}</h1>
