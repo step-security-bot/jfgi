be9800e1a38ee3540b35cd9da8db67a5141da261
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-02 14:28:03 -0400

    feat: update the design of visitor Counter
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/lib/components/VisitorCounter.svelte
+++ src/lib/components/VisitorCounter.svelte
@@ -1,17 +1,34 @@
 <script lang="ts" type="module">
   import type { Counter } from '$lib/types/Counter';
-
+  import { suffixes } from '$lib/suffixes';
   export let count: Counter;
+
+  // Extract the visitor count from the Counter object passed in;
+  const visitorCount = count.count;
+
+  // Split the visitor count into an array of digits;
+  const formattedCount = visitorCount.toLocaleString();
+
+  // Get a random message from suffixes
+  const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
 </script>
 
-<div class="mx-auto" id="Counter">
-  <p>
-    {#await count}
-      <span id="visitor-count" class="loading loading-infinity">...</span>
-    {:then count}
-      <span id="visitor-count" class="font-italic text-lg">{count.count}</span>
-    {:catch error}
-      <span id="visitor-count" class="text-lg font-bold text-red-600">{error}</span>
-    {/await}
-  </p>
-</div>
+<footer class="footer footer-center">
+  <div class="divider"></div>
+  <div class="" id="Counter">
+    <div class="badge badge-ghost badge-lg">
+      {formattedCount}
+      {suffix.adjective}
+      {visitorCount === 1 ? suffix.noun.singular : suffix.noun.plural}
+      {suffix.verb}
+    </div>
+  </div>
+  <aside class="prose flex-row">
+    <p>
+      Made with <span class="text-red-500">❤️</span> by
+      <a class="link link-info" href="https://github.com/AliSajid" target="_blank" rel="noopener"
+        >Ali Sajid Imami</a
+      >
+    </p>
+  </aside>
+</footer>
