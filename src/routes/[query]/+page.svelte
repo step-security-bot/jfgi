edf5829f92514559d456b7e1805b066f623e9c5c
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-02 23:12:24 -0400

    feat: add meta tags
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/routes/[query]/+page.svelte
+++ src/routes/[query]/+page.svelte
@@ -23,6 +23,15 @@
   <title>
     {data.siteName} -- Searching for {mySearchQuery}
   </title>
+  <meta
+    name="description"
+    content="{data.siteName} Search Page -- politely* tell people to Google things first"
+  />
+  <meta name="charset" content="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="nositelinkssearchbox" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="author" content="Ali Sajid Imami" />
 </svelte:head>
 
 <CountdownTimer {timer} />
