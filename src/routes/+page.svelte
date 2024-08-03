f67cd0b548fd5a77ac4b6b576e410a01caac1d38
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-02 20:40:00 -0400

    feat: add title element to the root/home page
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
   import Reprimand from '$lib/components/Reprimand.svelte';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+
+  $: isExplicit = data.explicit.isExplicit;
 </script>
+
+<svelte:head>
+  <title
+    >{isExplicit ? 'Just Fucking Google It! -- Home Page' : 'Just Google It! -- Home Page'}</title
+  >
+</svelte:head>
 
 <Reprimand />
