43e674fcd1bad0421472d288511a75371ba53e9e
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-03 22:01:52 -0500

    build: add a local SSL certificate
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit()],
+  plugins: [
+    enhancedImages(),
+    sveltekit(),
+    basicSsl({
+      name: 'localhost'
+    })
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
